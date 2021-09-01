673ff3757abdea7237df87e85694a3644846abf4
PrestonHarrod <prestonharrod@gmail.com>
2021-09-01 09:58:37 -0500

    added new welcome message

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Team 3's course app"/>
 </template>
 
 <script>
