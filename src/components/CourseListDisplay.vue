9f4c66f3cedb9b478e36ef270e7af01223949e40
Jared Torp <jared.torp@eagles.oc.edu>
2021-09-08 13:15:22 -0500

    Course Display Page: our page is not working due to CORS blocking our request. this is our first attempt

---
+++ src/components/CourseListDisplay.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <span> {{ course.name}} </span>
+    <span> {{course.hours}} </span>
+    <span> {{course["Course Number"]}} </span>
+    <router-link :to="{ name: 'edit', params: { id: list.id } }">
+      <span>edit</span>
+    </router-link>
+    <span>|</span>
+    <router-link :to="{ name: 'delete', params: { id: list.id } }">
+      <span>delete</span>
+    </router-link>
+    <span>)</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    course: Object
+  }
+}
+</script>
+
+<style></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <H1>Courses Home</H1>
+    <CourseListDisplay v-for="course in courses" :key="course.id" :list="course"/> 
+  </div>
+</template>
+
+<script>
+import CourseListDisplay from '@/components/CourseListDisplay.vue'
+import courseServices from '@/services/courseServices.js'
+
+
+export default {
+  components: {
+    CourseListDisplay,
+
+  },
+  data() {
+    return {
+      courses: [],
+
+
+    }
+  },
+  created() {
+      courseServices.getCourses() 
+      .then(response => {
+        this.courses = response.data.courses
+      })
+      .catch(error => {
+        this.message = error.data.message
+      })
+  }
+}
+</script>
+
+<style></style>
