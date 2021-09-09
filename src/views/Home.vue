6e827f83d8f5eb5c24e26c80dda67d8a011b698e
Jared Torp <jared.torp@eagles.oc.edu>
2021-09-08 21:22:12 -0500

    experimenting with the table, trying to make the data more readable

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,57 @@
+
 <template>
   <div>
+      <v-container>
+        <v-row>
+            <v-col>
+                <H1>Course List</H1>
+                <v-data-table
+                    :headers="headers"
+                    :items="courses"
+                    :items-per-page="25"
+                    class="elevation - 1"
+                    mobile-breakpoint="0"
+                    @click:row="selectRow"
+                >
+                </v-data-table>
+            </v-col>
+        </v-row>
+    </v-container>
  
-    <p>"hello"</p>
+    <!-- <p>"hello"</p>
     <span> Name | Hours | Course Number </span>
     <table> <CourseListDisplay v-for="course in courses" :key="course.id" :course="course"/> </table>
+  </div> -->
   </div>
 </template>
 
+
 <script>
-import CourseListDisplay from '@/components/CourseListDisplay.vue'
+//import CourseListDisplay from '@/components/CourseListDisplay.vue'
 import courseServices from '@/services/courseServices.js'
-
-
 export default {
-  components: {
-    CourseListDisplay,
-
-  },
-  data() {
-    return {
-      courses: [],
-      message : ""
-       
-      
-
-
-    }
-  },
+    components: {},
+    data() {
+        return {
+            dept: '',
+            courses: [],
+            headers: [
+                {
+                    text: 'Course Number',
+                    align: 'left',
+                    sortable: true,
+                    value: 'number',
+                    width: "150"
+                },
+                {
+                    text: 'Course Name',
+                    value: 'name',
+                    align: 'left',
+                    sortable: false,
+                },
+            ],
+        };
+    },
   created() {
       courseServices.getCourses() 
       .then(response => {
@@ -39,5 +64,7 @@ export default {
   }
 }
 </script>
+
+
 
 <style></style>
