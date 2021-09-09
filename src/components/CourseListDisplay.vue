741b47acae78df31ef574964639838f65f418885
Logan Christopher <lchris20@gmail.com>
2021-09-08 22:10:58 -0500

    added Jareds CourseListDisplay to my branch

---
+++ src/components/CourseListDisplay.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <tr> 
+          <th>{{course.Name}}</th>
+          <th>{{course.Hours}}</th>
+          <th>{{course["Course Number"]}}</th>
+        <th> <router-link :to="{ name: 'edit', params: { id: course.id } }">
+      <span>edit</span>
+    </router-link></th>
+    <th><router-link :to="{ name: 'delete', params: { id: course.id } }">
+      <span>delete</span> 
+      </router-link> </th>
+    </tr>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    course: Object
+  }
+}
+</script>
+
+<style></style>
