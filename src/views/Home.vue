8455ae7b71fde1ed02afc7480f0b1636f83c5c94
Preston Harrod <prestonharrod@gmail.com>
2021-09-10 19:42:26 -0500

    update home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,47 @@
+
 <template>
   <div>
+    <h1>Course Home</h1>
+         <table class="center">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Hours</th>
+                    <th>Course Number</th>
+                    <th>View Course</th>
+                     <th>Delete</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="course in courses" :key="course.id" :course="course">
+                    <td>{{course.Name}}</td>
+                    <td>{{course.Hours}}</td>
+                    <td>{{course["Course Number"]}}</td>
+                    <td><v-btn large outlined>View Course</v-btn></td>
+                   <td><v-btn large outlined>Delete</v-btn></td>
+                </tr>
+            </tbody>
+        </table>
  
-    <p>"hello"</p>
-    <span> Name | Hours | Course Number </span>
-    <table> <CourseListDisplay v-for="course in courses" :key="course.id" :course="course"/> </table>
+
   </div>
 </template>
+
 
 <script>
 //import CourseListDisplay from '@/components/CourseListDisplay.vue'
 import courseServices from '@/services/courseServices.js'
 export default {
-  components: {
-    CourseListDisplay,
-  },
-  data() {
-    return {
-      courses: [],
-      message : ""
-       
-      
-    }
-  },
+    components: {},
+    data() {
+        return {
+            courses: [],
+        };
+    },
   created() {
       courseServices.getCourses() 
       .then(response => {
         this.courses = response.data
-        console.log("here")
       })
       .catch(error => {
         console.log(error)
@@ -35,4 +50,29 @@ export default {
 }
 </script>
 
-<style></style>
+
+
+<style>
+table {
+  border-collapse: collapse;
+  width: 50%;
+
+}
+
+th, td {
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even){background-color: #f2f2f2}
+
+th {
+  background-color: #811429
+;
+  color: white;
+}
+.center {
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
