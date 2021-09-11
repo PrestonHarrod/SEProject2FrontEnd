d4b69659bdd96fb6283fc858f7b1af66ca81c795
Preston Harrod <prestonharrod@gmail.com>
2021-09-10 19:39:24 -0500

    add home view

---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+ 
+    <p>"hello"</p>
+    <span> Name | Hours | Course Number </span>
+    <table> <CourseListDisplay v-for="course in courses" :key="course.id" :course="course"/> </table>
+  </div>
+</template>
+
+<script>
+//import CourseListDisplay from '@/components/CourseListDisplay.vue'
+import courseServices from '@/services/courseServices.js'
+export default {
+  components: {
+    CourseListDisplay,
+  },
+  data() {
+    return {
+      courses: [],
+      message : ""
+       
+      
+    }
+  },
+  created() {
+      courseServices.getCourses() 
+      .then(response => {
+        this.courses = response.data
+        console.log("here")
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+}
+</script>
+
+<style></style>
