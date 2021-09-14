015c2cf61a239099fb146cc67bb3ac478d0a74e8
PrestonHarrod <prestonharrod@gmail.com>
2021-09-14 14:43:49 -0500

    working delete button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,39 @@
 <template>
   <div>
     <h1>Course Home</h1>
-    <td><button @click="$router.push('/api/courses/courseadd')">Add Course</button></td>
+    <td>
+      <button @click="$router.push('/api/courses/courseadd')">
+        Add Course
+      </button>
+    </td>
 
-         <table class="table table-striped table-bordered">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Hours</th>
-                    <th>Course Number</th>
-                    <th>Course Details</th>
-                     <th>Delete</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="course in courses" :key="course.id" :course="course">
-                    <td>{{course.Name}}</td>
-                    <td>{{course.Hours}}</td>
-                    <td>{{course["Course Number"]}}</td>
-                    <router-link :to="{name: 'view', params: {id: course.id}}"><span>View Course</span></router-link>
-                    <button class="delete-btn" @click="doDelete">Delete</button>
-                    <confirm-dialog ref="confirmDialog"></confirm-dialog>
-                </tr>
-            </tbody>
-        </table>
- 
-
+    <table class="table table-striped table-bordered">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Hours</th>
+          <th>Course Number</th>
+          <th>Course Details</th>
+          <th>Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="course in courses" :key="course.id" :course="course">
+          <td>{{course.Name}}</td>
+          <td>{{course.Hours}}</td>
+          <td>{{course["Course Number"]}}</td>
+          <router-link :to="{name: 'view', params: {id: course.id}}"
+            ><span>View Course</span></router-link
+          >
+          <button class="delete-btn" @click="doDelete(courses, course.id)">
+            Delete
+          </button>
+          <confirm-dialog ref="confirmDialog"></confirm-dialog>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
-
 
 <script>
 //import CourseListDisplay from '@/components/CourseListDisplay.vue'
@@ -42,7 +47,7 @@ export default {
         };
     },
   created() {
-      courseServices.getCourses() 
+      courseServices.getCourses()
       .then(response => {
         this.courses = response.data
       })
@@ -51,50 +56,58 @@ export default {
       })
   },
   methods: {
-    async doDelete() {
+    async doDelete(courses, id) {
             const ok = await this.$refs.confirmDialog.show({
-                title: 'Delete Page',
+                title: 'Delete Course',
                 message: 'Are you sure you want to delete this course? It cannot be undone.',
                 okButton: 'Delete Forever',
             })
             // If you throw an error, the method will terminate here unless you surround it wil try/catch
             if (ok) {
-                alert('You have successfully deleted this course.')
+                courseServices.deleteCourse(id)
+      .then(() => {
+        this.courses.forEach((course,i) => {
+          if (course.id == id) {
+            this.courses.splice(i, 1);
+          }
+        })
+
+        })
             } else {
                 alert('You chose not to delete this course. Doing nothing now.')
             }
           }
-      }
+      },
 }
 </script>
-
-
 
 <style>
 table {
   border-collapse: collapse;
   width: 100%;
 }
-th, td {
+th,
+td {
   text-align: left;
   padding: 8px;
 }
-tr:nth-child(even){background-color: #f2f2f2}
+tr:nth-child(even) {
+  background-color: #f2f2f2;
+}
 th {
-  background-color: #811429
-;
+  background-color: #811429;
   color: white;
 }
 .delete-btn {
-    margin-left: 100px;
-    padding: 0.5em 1em;
-    background-color: #eccfc9;
-    color: #c5391a;
-    border: 2px solid #ea3f1b;
-    border-radius: 5px;
-    font-weight: bold;
-    font-size: 16px;
-    text-transform: uppercase;
-    cursor: pointer;
+  margin-left: 100px;
+  padding: 0.5em 1em;
+  background-color: #eccfc9;
+  color: #c5391a;
+  border: 2px solid #ea3f1b;
+  border-radius: 5px;
+  font-weight: bold;
+  font-size: 12px;
+  text-transform: uppercase;
+  cursor: pointer;
 }
 </style>
