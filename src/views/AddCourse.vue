da58a1189d89505e621ae6c6a27e78aa07194c7c
JackHannahOC <70724833+JackHannahOC@users.noreply.github.com>
2021-09-14 22:19:16 -0500

    Added formatting and style to AddCourse page

--- src/views/AddCourse.vue
+++ src/views/AddCourse.vue
@@ -1,30 +1,41 @@
 <template>
   <div>
-    <H1>Add Course</H1>
-    
+    <H1 style="background-color: #811429; color:#f2f2f2">Add Course</H1>
+  
 
     <form @submit.prevent="addCourse">
-      Department:
-      <input v-model="course.Dept" type="text" id="courseDept">  
+      <table>
+      <tr>
+      <td style="height: 30px; width: 80px; display: inline-block">  Department: </td>    
+      <td style="height: 30px; display: inline-block" > <input v-model="course.Dept" type="text" id="courseDept"> </td>    
+      </tr>
+      <tr>
+      <td style="height: 30px; width: 80px; display: inline-block"> Course Number: </td>
+      <td style="height: 30px;  display: inline-block"><input v-model="course['Course Number']" type="text" id="courseNumber"> </td>
+      </tr>
+      <tr>
+      <td style="height: 30px; width: 80px; display: inline-block"> Level: </td>
+      <td style="height: 30px;  display: inline-block"> <input v-model="course.Level" type="text" id="courseLevel"> </td> 
+      </tr>
+      <tr>
+      <td style="height: 30px; width: 80px; display: inline-block"> Hours: </td>
+      <td style="height: 30px;  display: inline-block"> <input v-model="course.Hours" type="text" id="courseHours"> </td>
+      </tr>
+      <tr>
+      <td style="height: 30px; width: 80px; display: inline-block"> Course Name: </td>
+      <td style="height: 30px; display: inline-block"> <input v-model="course.Name" type="text" id="courseName"> </td>
+      </tr>
+      <tr>
+      <td style="width: 80px; display: inline-block"> Description: </td>
       <br>
-      Course Number:
-      <input v-model="course['Course Number']" type="text" id="courseNumber">   
-      <br>
-      Level:
-      <input v-model="course.Level" type="text" id="courseLevel">   
-      <br>
-      Hours:
-      <input v-model="course.Hours" type="text" id="courseHours">
-      <br>
-      Course Name:
-      <input v-model="course.Name" type="text" id="courseName">
-      <br>
-      Description:
-      <input v-model="course.Description" type="text" id="courseDescription">
-      <br>
+      <textarea v-model="course.Description" type="text" style='height:80px; width:500px; white-space: pre-line;' id="courseDescription"></textarea> 
+      </tr>
+      <br> <br>
+      <tr>
       <button name="submit" v-on:click.prevent="addCourse()">Submit</button>
       <button name="cancel" v-on:click.prevent="cancel()">Cancel</button>
-
+      </tr>
+      </table>
     </form>
   </div>
 </template>
@@ -42,14 +53,14 @@ export default {
      
         courseServices.addCourse(this.course)
         .then(() => {
-          this.$router.push({ name: 'course' })
+          this.$router.push({ name: 'view' })
         })
         .catch(error => {
           console.log(error)
         })
     },
     cancel() {
-      this.$router.push({ name: 'course' })
+      this.$router.push({ name: 'view' })
     },
   }
 }
