1294bee86dbca75b285b4484079d6ed7892f46c4
PrestonHarrod <prestonharrod@gmail.com>
2021-09-15 20:32:44 -0500

    works if you npm run build

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
-
+module.exports = {
   devServer: {
     host: 'localhost',
   },
-  publicPath: process.env.NODE_ENV === 'development' ? '/' : '/courses/',
-  transpileDependencies: ['vuetify'],
+  publicPath: process.env.NODE_ENV === 'development' ? '/' : '/webpages/courses/'
+  
 };
